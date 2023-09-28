f5f1304211ea11a82bbb6e6c7277af7cab9cec84
Rafael A.R da Rosa <rodrigues.rafaelantonio@gmail.com>
2023-09-27 21:11:33 -0300

    continuação da Aula

---
+++ Seção 2 -  JavaScript para iniciantes/12 - Web_e_node/index.js
@@ -0,0 +1,3 @@
+        //console pra começar
+        console.log('Ahhhhhhhhhhhhhhhh')
+        console.log('vai ser mostrado no console')
---
+++ Seção 2 -  JavaScript para iniciantes/13 - variaveis_let.js
@@ -0,0 +1,5 @@
+let nome1 = 'João'
+let nome2 = 'Maria'
+
+console.log(`${nome1} nasceu em 1989`)
+console.log(`em 2000 ${nome2} casou com ${nome1}`)
